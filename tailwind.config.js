48b23eb9cc4662cd86945023fa5c05c2c3be9de9
Silviu Miron <silviu_miron_90@outlook.com>
2024-02-08 22:33:59 +0200

    Feat : Add new style guide in tailwind config .

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,31 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  colors: {
+    // primary
+
+    "Very Dark Blue": "hsl(243, 87%, 12%)",
+    "Desaturated Blue": "hsl(238, 22%, 44%)",
+
+    // Accent
+
+    "Bright Blue": "hsl(224, 93%, 58%)",
+    "Moderate Cyan": "hsl(170, 45%, 43%)",
+
+    // Neutral
+    "Light Grayish Blue": "hsl(240, 75%, 98%)",
+    "Light Gray": "hsl(0, 0%, 75%)",
+  },
+  body: {
+    // paragraph
+    "font size": "16px",
+  },
+  fonts: {
+    // headings,call-to-action, header navigation
+    "font-family": "'Raleway', sans-serif",
+
+    // Body
+
+    "font-family": "'Open Sans', sans-serif",
+  },
 };
