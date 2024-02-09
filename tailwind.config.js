3f88bcc9697ddf446ad1d11aa3c7deafcad6d1e4
Silviu Miron <silviu_miron_90@outlook.com>
2024-02-09 23:24:14 +0200

    Feat : Build menu.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,26 @@
 module.exports = {
   content: ["./docs/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // primary
+
+        "Very-dark-blue": "#070439",
+        "Desaturated-blue": "hsl(238, 22%, 44%)",
+
+        // Accent
+
+        "Bright-Blue": "hsl(224, 93%, 58%)",
+        "Moderate Cyan": "hsl(170, 45%, 43%)",
+
+        // Neutral
+        "Light Grayish Blue": "hsl(240, 75%, 98%)",
+        "Light Gray": "hsl(0, 0%, 75%)",
+      },
+    },
   },
   plugins: [],
-  colors: {
-    // primary
 
-    "Very-Dark-Blue": "hsl(243, 87%, 12%)",
-    "Desaturated Blue": "hsl(238, 22%, 44%)",
-
-    // Accent
-
-    "Bright Blue": "hsl(224, 93%, 58%)",
-    "Moderate Cyan": "hsl(170, 45%, 43%)",
-
-    // Neutral
-    "Light Grayish Blue": "hsl(240, 75%, 98%)",
-    "Light Gray": "hsl(0, 0%, 75%)",
-  },
   body: {
     // paragraph
     "font size": "16px",
