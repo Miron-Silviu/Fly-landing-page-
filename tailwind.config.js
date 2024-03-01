825a4159b1ef3de8230c6e26a0bb0dca2d220b30
Silviu Miron <silviu_miron_90@outlook.com>
2024-03-01 23:53:05 +0200

    Feat : Modify taiwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,20 @@ module.exports = {
   content: ["./docs/**/*.{html,js}"],
   theme: {
     screens: {
-      xs: "320px",
-      sm: "640px",
-      // => @media (min-width: 640px) { ... }
+      xs: { min: "220", max: "320px" },
+      sm: { min: "321px", max: "640px" },
+      // => @media (min-width: 640px and max-width: 767px) { ... }
 
-      md: "768px",
-      // => @media (min-width: 768px) { ... }
+      md: { min: "641px", max: "1024px" },
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
 
-      lg: "1024px",
-      // => @media (min-width: 1024px) { ... }
+      lg: { min: "1024px", max: "1279px" },
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
 
-      xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
+      xl: { min: "1280px", max: "1535px" },
+      // => @media (min-width: 1280px and max-width: 1535px) { ... }
 
-      "2xl": "1536px",
+      "2xl": { min: "1536px" },
       // => @media (min-width: 1536px) { ... }
     },
     extend: {
